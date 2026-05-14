3c96f824fb40bb9c382a0c9264bf2e71a7822b59
raineycat <raineybc9@gmail.com>
2026-05-14 14:36:23 +0100

    more web stuff!

--- src/main/kotlin/uk/reddust/hostess/WebInterface.kt
+++ src/main/kotlin/uk/reddust/hostess/WebInterface.kt
@@ -1,6 +1,7 @@
 package uk.reddust.hostess
 
 import io.github.oshai.kotlinlogging.KotlinLogging
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
@@ -10,6 +11,10 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.sse.*
+import io.ktor.server.velocity.Velocity
+import io.ktor.server.velocity.respondTemplate
+import org.apache.velocity.runtime.RuntimeConstants
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader
 import java.util.*
 
 class WebInterface(val port: Int) {
@@ -19,11 +24,31 @@ class WebInterface(val port: Int) {
     fun start() {
         embeddedServer(Netty, port = port) {
             install(SSE)
-            install(StatusPages)
+
+            install(StatusPages) {
+                exception<Throwable> { call, cause ->
+                    if (cause is NotFoundException) {
+                        call.respondTemplate("templates/404.html.vsl", mutableMapOf("msg" to cause.localizedMessage))
+                        return@exception
+                    }
+
+                    call.respondText(text = "500: $cause" , status = HttpStatusCode.InternalServerError)
+                    throw cause
+                }
+            }
+
+            install(Velocity) {
+                setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath")
+                setProperty("classpath.resource.loader.class", ClasspathResourceLoader::class.java.name)
+            }
 
             routing {
                 route("/clients") {
                     clientRoutes()
+                }
+
+                get("/pico") {
+                    call.respondResource("vendor/pico.amber.min.css")
                 }
             }
         }.start(wait = true)
@@ -39,30 +64,31 @@ class WebInterface(val port: Int) {
 
     private fun Route.clientRoutes() {
         get {
-            call.respondText(clients.map { it.uuid }.joinToString("\n"))
+            call.respondTemplate("templates/list.html.vsl", mutableMapOf("clients" to clients))
         }
 
         route("/{id}") {
             fun RoutingContext.getClient(): ClientConn {
-                val id = call.parameters["id"] ?: throw NotFoundException()
+                val id = call.parameters["id"] ?: throw NotFoundException("No client ID supplied")
                 val uuid = runCatching {
                     UUID.fromString(id)
-                }.getOrElse { throw NotFoundException() }
+                }.getOrElse { throw NotFoundException("Invalid client ID format") }
 
-                return clients.find { it.uuid == uuid } ?: throw NotFoundException()
+                return clients.find { it.uuid == uuid } ?: throw NotFoundException("The given client ID doesn't exist")
             }
 
             get {
-                call.respondText { getClient().name }
+                call.respondTemplate("templates/client.html.vsl", mutableMapOf("client" to getClient()))
             }
 
             get("/log") {
-                call.respondText { getClient().logHistory.joinToString("\n") }
+                call.respondTemplate("templates/log.html.vsl", mutableMapOf("client" to getClient()))
             }
 
             post("/lua") {
                 val formBody = call.receiveParameters()
                 getClient().sendLua(formBody["code"].toString())
+                call.respondRedirect("/clients/${getClient().uuid}")
             }
         }
     }
